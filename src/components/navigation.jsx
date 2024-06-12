d2ee543a6d264efcd7744fa3a51a5a07ad42632c
Anmol kumar <anmolkumar31884@gmail.com>
2024-06-12 13:52:07 +0530

    location functionalities added

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,18 +1,21 @@
-// import NavData from "../data/navData";
-// import NavigationItem from "./navigationItem";
 import { SiSwiggy } from "react-icons/si";
 import { FaLocationArrow } from "react-icons/fa";
 import { FaAngleDown } from "react-icons/fa6";
-
 import { CiSearch } from "react-icons/ci";
 import { IoPersonCircleSharp } from "react-icons/io5";
-import { useState, useRef, useEffect } from "react";
-
+import { useState, useRef, useEffect, useContext } from "react";
+import UserContext from "../context/userContext";
 import '../style/navigation.css'
 function Navigation() {
 
     const [isVisible, setIsVisible] = useState(false);
     const divRef = useRef(null);
+    const { suggestedLocation, setsuggestedLocation, usercoordinates, setUserCoordinates } = useContext(UserContext)
+
+    const [dataForLocation, setDataForLocation] = useState([])
+    useEffect(() => {
+        setDataForLocation(suggestedLocation)
+    }, [suggestedLocation])
 
     const handleDivClick = () => {
         setIsVisible(true);
@@ -31,7 +34,60 @@ function Navigation() {
         };
     }, []);
 
-    const [inputvalue, setInputValue] = useState('')
+
+
+
+    const [location, setLocation] = useState('')
+    const [suggestionVisible, setSuggestionVisible] = useState(false)
+    const loctionHandler = (e) => {
+        const initialInputValue = e.target.value;
+        setLocation(initialInputValue)
+
+        const modifiedValue = initialInputValue.replace(' ', '+')
+        // console.log(modifiedValue)
+        locationUpdate(modifiedValue)
+
+    }
+    async function locationUpdate(modifiedValue) {
+        const locationUrl = `https://www.swiggy.com/dapi/misc/place-autocomplete?input=${modifiedValue}&type=`
+        try {
+            const response = await fetch(locationUrl)
+            const locationOutput = await response.json()
+            setsuggestedLocation(locationOutput)
+        }
+        catch (err) {
+            console.log(err)
+
+        }
+    }
+    const locationList = suggestedLocation.data
+    console.log(locationList)
+
+
+
+
+    const handlingGeoPostion = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition)
+        }
+        else {
+            alert('Location is not Supported in your system')
+        }
+    }
+    const showPosition = (position) => {
+        const UserCordinates = {
+
+            long: position.coords.longitude,
+            lat: position.coords.latitude,
+            // setUserCoordinates(position)
+
+        }
+        setUserCoordinates(UserCordinates)
+    }
+
+
+
+    console.log(usercoordinates)
 
 
     return (
@@ -47,12 +103,54 @@ function Navigation() {
                 <FaAngleDown className="color"></FaAngleDown>
 
 
-                <div className="location-floating-cont">
+                <div className="floating-location-section">
 
                     {isVisible && (
-                        <div ref={divRef} >
-                            <FaLocationArrow className="color search " />
-                            <span>Setup your precise location</span>
+                        <div ref={divRef} className="location-floating-cont" >
+                            <div>
+
+                                <div className="location-input">
+                                    <FaLocationArrow className="location-icon-search-section" />
+
+
+                                    <input onChange={loctionHandler} value={location} className="location-input-field" type="text" placeholder="Search for Area StreetName..." />
+                                </div>
+                            </div>
+                            <hr className="hr" />
+                            <div className="locate-me" onClick={handlingGeoPostion}>
+                                <FaLocationArrow className="location-icon-search-section" />
+                                <span>Locate me using GPS</span>
+                            </div>
+
+
+                            <div>
+                                {
+                                    Array.isArray(locationList) && (
+                                        locationList.map((item) => (
+                                            <div className="suggested-location-section-cont">
+                                                <hr className="hr" />
+                                                <div className="suggested-location">
+                                                    <div>
+                                                        <FaLocationArrow className="location-low-color-arrow"></FaLocationArrow>
+
+                                                    </div>
+                                                    <div className="both-location-text">
+
+                                                        <div className="location-primary-text" >
+                                                            {item.structured_formatting.main_text}
+                                                        </div>
+                                                        <div className="location-secondary-text">
+                                                            {
+                                                                item.structured_formatting.secondary_text
+                                                            }
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))
+                                    )
+                                }
+                            </div>
                         </div>
                     )}
 
--- src/context/contextProvider.js
+++ src/context/contextProvider.js
@@ -7,16 +7,16 @@ function UserContextProvider({ children }) {
     const [whatsOnYourMind, setWhatsonyourmind] = useState([])
     const [resturantChains, setResturantsChain] = useState([])
     const [loading, setLoading] = useState(false)
-    const[slider,setSlider]=useState(true)
-    const [foodDelivery,setFoodDelivery]=useState([])
-    const [bestplace,setBestPlace]=useState([])
-    const [bestCuisines,setBestCuisines]=useState([])
-    const[swiggyDownload,setSwiggyDownload]=useState({})
+    const [slider, setSlider] = useState(true)
+    const [foodDelivery, setFoodDelivery] = useState([])
+    const [bestplace, setBestPlace] = useState([])
+    const [bestCuisines, setBestCuisines] = useState([])
+    const [swiggyDownload, setSwiggyDownload] = useState({})
 
     // const url2 = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.59080&lng=85.13480&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
     // const url = 'https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING'
 
-    const url='https://foodfire.onrender.com/api/restaurants?lat=25.59080&lng=85.13480&page_type=DESKTOP_WEB_LISTING'
+    const url = 'https://foodfire.onrender.com/api/restaurants?lat=25.59080&lng=85.13480&page_type=DESKTOP_WEB_LISTING'
     // const url2 = 'https://www.swiggy.com/mapi/homepage/getCards?lat=25.5940947&lng=85.1375645'
 
     // const itemUrl='https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=25.59080&lng=85.13480&&submitAction=ENTER&restaurantId=725082'
@@ -27,34 +27,34 @@ function UserContextProvider({ children }) {
             const data = await response.json()
             setWhatsonyourmind(data.data.cards[0].card.card.imageGridCards.info)
             setFoodDelivery(data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
-            
-            
+
+
             // console.log('hello')
             // setWhatsonyourmind(data?.data?.cards[0].card.card.imageGridCards.info)
             // console.log(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
             // setResturantsChain(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
-            
+
         }
         catch (err) {
             console.log(err)
         }
-        
 
-        }
-        
-        
-        async function fetchData2() {
-            try {
-                const response2 = await fetch(url)
-                const data2 = await response2.json()
-                // console.log(data2?.data?.success.cards[1].gridWidget.gridElements.infoWithStyle.restaurants)
-                // console.log(data2.data.cards[1].card)
-                // setResturantsChain(data2?.data?.success.cards[1].gridWidget.gridElements.infoWithStyle.restaurants)
-                setResturantsChain(data2.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
-                setBestPlace(data2.data.cards[6].card.card.brands)
-                setBestCuisines(data2.data.cards[7].card.card.brands)
-                // console.log(data2.data.cards[6].card.card.brands)
-                setSwiggyDownload(data2.data.cards[9].card.card)
+
+    }
+
+
+    async function fetchData2() {
+        try {
+            const response2 = await fetch(url)
+            const data2 = await response2.json()
+            // console.log(data2?.data?.success.cards[1].gridWidget.gridElements.infoWithStyle.restaurants)
+            // console.log(data2.data.cards[1].card)
+            // setResturantsChain(data2?.data?.success.cards[1].gridWidget.gridElements.infoWithStyle.restaurants)
+            setResturantsChain(data2.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+            setBestPlace(data2.data.cards[6].card.card.brands)
+            setBestCuisines(data2.data.cards[7].card.card.brands)
+            // console.log(data2.data.cards[6].card.card.brands)
+            setSwiggyDownload(data2.data.cards[9].card.card)
             console.log(bestplace)
             // setFoodDelivery()
             console.log(data2.data.cards)
@@ -63,6 +63,13 @@ function UserContextProvider({ children }) {
             console.log(err)
         }
     }
+
+
+
+
+
+    const [suggestedLocation, setsuggestedLocation] = useState([])
+    const[usercoordinates,setUserCoordinates]=useState({})
 
     const value = {
         whatsOnYourMind,
@@ -78,9 +85,13 @@ function UserContextProvider({ children }) {
         foodDelivery,
         setFoodDelivery,
         bestplace,
-        setBestPlace,setBestCuisines,bestCuisines,
-        swiggyDownload
-    
+        setBestPlace, setBestCuisines, bestCuisines,
+        swiggyDownload,
+        suggestedLocation,
+        setsuggestedLocation,
+        usercoordinates,setUserCoordinates
+
+
 
     }
 
