5ad849fd6bf4c21584354b74dabcef32f2cd73ea
Anmol kumar <anmolkumar31884@gmail.com>
2024-06-14 23:27:10 +0530

    resturant page added

--- src/pages/resturants.jsx
+++ src/pages/resturants.jsx
@@ -1,6 +1,37 @@
-function Resturants(){
-    return(
-        <h1>Resturant</h1>
-    )
-}
+import Navigation from "../components/navigation"
+import UserContext from "../context/userContext"
+import { useContext } from "react"
+
+    function Resturants() {
+        const {collectionItemData} = useContext(UserContext)
+        const itemHeading=collectionItemData
+        console.log(itemHeading)
+        // const itemSubHeading=
+        // const resturantsNo=
+        return (
+            <div>
+                <Navigation></Navigation>
+                <div>
+                    <div>
+                        {/* <h1>{itemHeading}</h1> */}
+                        <p>
+
+                        </p>
+
+                        <div>
+
+                        </div>
+
+                        <h2>
+
+                        </h2>
+
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 export default Resturants
