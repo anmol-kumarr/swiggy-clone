74e1cb770836f720e3f5f584159fb0c17b5a62c3
Anmol kumar <anmolkumar31884@gmail.com>
2024-06-14 23:26:05 +0530

    whats on your mind changed

--- src/components/whatsInyourMind.jsx
+++ src/components/whatsInyourMind.jsx
@@ -6,6 +6,7 @@ import Slider from "react-slick";
 import '../style/whatson.css'
 import { FaArrowRight } from "react-icons/fa";
 import Scrollbar from "./scrollbar";
+import { useNavigate } from "react-router";
 function WhatsOnYour() {
     const { whatsOnYourMind, setSlider, slider, setCollectionID, setCollectionName, fetchItemCollection } = useContext(UserContext)
 
@@ -15,11 +16,14 @@ function WhatsOnYour() {
 
         setSlider(true)
     }, [])
+    const navigate=useNavigate()
     function navigateToCollesction(entityId, item) {
-        const CollectionID = entityId.slice(36, 41)
+        const CollectionID = entityId.slice(35, 40)
+        console.log(CollectionID)
         setCollectionID(CollectionID)
         setCollectionName(item)
         fetchItemCollection()
+        // navigate('./resturants')
 
     }
     // console.log(whatsOnYourMind)
@@ -38,7 +42,7 @@ function WhatsOnYour() {
                         (
                             // <OnYourMindCard key={arr.id} data={arr} ></OnYourMindCard>)
 
-                            <div onClick={() => navigateToCollesction(arr.entityId, arr.action.text)} className='card-img' key={arr.id}>
+                            <div onClick={() => navigateToCollesction(arr.action.link, arr.action.text)} className='card-img' key={arr.id}>
                                 <img src={url + arr.imageId} alt="" height={150} />
                             </div>
 
