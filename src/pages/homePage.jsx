4dc314836f5cc205f1101c26258f5a87fc008eb4
Anmol kumar <anmolkumar31884@gmail.com>
2024-07-27 00:55:02 +0530

    shimmer effect added

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -9,18 +9,65 @@ import ResturantChains from '../components/resturentChains';
 import FoodDelivery from '../components/fooddelivery';
 import Best from '../components/swiggyBest';
 import Footer from '../components/footer';
+import { useEffect, useState } from 'react';
 
 function HomePage() {
+    const [homePageData, setHomePageData] = useState([])
+    const [fail, setFail] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [whatsInyourMind,setwhatsInYourMind]=useState([])
+    const[resturantChains,setResturnatsChains]=useState([])
+    const[foodDelivery,setFoodDelivery]=useState([])
+    useEffect(() => {
+        const url = 'https://foodfire.onrender.com/api/restaurants?lat=25.59080&lng=85.13480&page_type=DESKTOP_WEB_LISTING'
+        const ftechHomeData = async () => {
+            try {
+                setLoading(true)
+                const response = await fetch(url)
+                const data = await response.json()
+                console.log(data)
+                setwhatsInYourMind(data.data.cards[0].card.card.imageGridCards.info)
+                setResturnatsChains(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+                setFoodDelivery(data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
+                
+            
+
+                setLoading(false)
+            }
+            catch (err) {
+                console.log(err)
+                setFail(true)
+            }
+
+        }
+        ftechHomeData()
+        
+        
+    }, [])
+    // const whatsOnYourMind=homePageData.data.cards[0].card.card.imageGridCards.info;
+    // const resturnatsChains=homePageData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+    // const foodDelivery
+    // console.log(whatsInyourMind)
     return (
         <div>
-            <Navigation></Navigation>
-            <Hero></Hero>
-            <WhatsOnYour></WhatsOnYour>
-            <ResturantChains ></ResturantChains>
-            {/* <Btn ></Btn> */}
-            <FoodDelivery></FoodDelivery>
-            <Best></Best>
-            <Footer></Footer>
+            {
+                fail === false ? (<div>
+
+                    <Navigation></Navigation>
+                    <Hero></Hero>
+                    <WhatsOnYour  WhatsOnYourMindData={whatsInyourMind}  loading={loading}></WhatsOnYour>
+                    <ResturantChains resturantChains={resturantChains} loading={loading} ></ResturantChains>
+                    <FoodDelivery foodDelivery={foodDelivery} loading={loading} ></FoodDelivery>
+                    <Best></Best>
+                    <Footer></Footer>
+                </div>
+                ) : (
+                    <div>
+                        <h1>Some Thing went wrong</h1>
+                    </div>
+                )
+            }
+
         </div>
     )
 }
