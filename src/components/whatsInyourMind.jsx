7c922ed927753067a8303fea5bac952710de26b6
Anmol kumar <anmolkumar31884@gmail.com>
2024-07-27 00:48:12 +0530

    shimmer effect added

--- src/components/whatsInyourMind.jsx
+++ src/components/whatsInyourMind.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import UserContext from "../context/userContext";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,26 +7,21 @@ import '../style/whatson.css'
 import { FaArrowRight } from "react-icons/fa";
 import Scrollbar from "./scrollbar";
 import { useNavigate } from "react-router";
-function WhatsOnYour() {
-    const { whatsOnYourMind, setSlider, slider, setCollectionID, setCollectionName, fetchItemCollection } = useContext(UserContext)
+
+import { ShimmerCircularImage } from "react-shimmer-effects";
+
+function WhatsOnYour({ loading,WhatsOnYourMindData }) {
+    // const { whatsOnYourMind, setSlider, slider, setCollectionID, setCollectionName, fetchItemCollection } = useContext(UserContext)
+    const[slider,setSlider]=useState(false)
+    console.log(WhatsOnYourMindData)
 
     // console.log(whatsOnYourMind)
     const url = "https://media-assets.swiggy.com/"
+
     useEffect(() => {
 
         setSlider(true)
     }, [])
-    const navigate=useNavigate()
-    function navigateToCollesction(entityId, item) {
-        const CollectionID = entityId.slice(35, 40)
-        console.log(CollectionID)
-        setCollectionID(CollectionID)
-        setCollectionName(item)
-        fetchItemCollection()
-        navigate('./resturants')
-
-    }
-    // console.log(whatsOnYourMind)
     return (
 
 
@@ -34,22 +29,35 @@ function WhatsOnYour() {
             <h2>What's on your mind?</h2>
             <div className="slider-container">
 
-                {/* <Slider {...settings}> */}
+            
                 <Scrollbar slider={slider} >
 
+
                     {
-                        whatsOnYourMind.map((arr) =>
-                        (
-                            // <OnYourMindCard key={arr.id} data={arr} ></OnYourMindCard>)
-
-                            <div onClick={() => navigateToCollesction(arr.action.link, arr.action.text)} className='card-img' key={arr.id}>
-                                <img src={url + arr.imageId} alt="" height={150} />
+                        loading === true ? (
+                            <div className="scroll-container">
+                                {Array(9).fill().map((_, index) => (
+                                    <ShimmerCircularImage key={index} size={100} caption />
+                                ))}
                             </div>
+                        ) : (
+                            <div className="scroll-container">
+                                {
+                                    WhatsOnYourMindData.map((arr) =>
+                                    (
+                                    
 
-                        ))
+                                        <div className='card-img' key={arr.id}>
+                                            <img src={url + arr.imageId} alt="" height={150} />
+                                        </div>
+
+                                    ))
+                                }
+                            </div>
+                        )
                     }
+
                 </Scrollbar>
-                {/* </Slider> */}
 
             </div>
 
