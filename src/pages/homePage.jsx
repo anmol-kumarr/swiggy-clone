3d7609c0ea7fbd5b397d6899ad1d4901a902f467
Anmol kumar <anmolkumar31884@gmail.com>
2024-07-30 22:20:50 +0530

    geolocation search feature added

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -10,6 +10,11 @@ import FoodDelivery from '../components/fooddelivery';
 import Best from '../components/swiggyBest';
 import Footer from '../components/footer';
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+
+
+// https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.6102763&lng=85.1338404&restaurantId=88781&submitAction=ENTER
 
 function HomePage() {
     const [homePageData, setHomePageData] = useState([])
@@ -18,37 +23,45 @@ function HomePage() {
     const [whatsInyourMind,setwhatsInYourMind]=useState([])
     const[resturantChains,setResturnatsChains]=useState([])
     const[foodDelivery,setFoodDelivery]=useState([])
+    const cityGeoLocation=useSelector((state)=>state.location[0])
+    const long=cityGeoLocation.long
+    const lat=cityGeoLocation.lat
+ 
+    const url = `https://foodfire.onrender.com/api/restaurants?lat=${lat}&lng=${long}&page_type=DESKTOP_WEB_LISTING`
+
+    
     useEffect(() => {
-        const url = 'https://foodfire.onrender.com/api/restaurants?lat=25.59080&lng=85.13480&page_type=DESKTOP_WEB_LISTING'
-
-        
-// https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.6102763&lng=85.1338404&restaurantId=88781&submitAction=ENTER
-
-
-        const ftechHomeData = async () => {
-            try {
-                setLoading(true)
-                const response = await fetch(url)
-                const data = await response.json()
-                // console.log(data)
-                setwhatsInYourMind(data.data.cards[0].card.card.imageGridCards.info)
-                setResturnatsChains(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
-                setFoodDelivery(data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
-                
-            
-
-                setLoading(false)
-            }
-            catch (err) {
-                console.log(err)
-                setFail(true)
-            }
-
-        }
-        ftechHomeData()
-        
-        
+        fetchHomeData()
     }, [])
+
+
+    useEffect(()=>{
+        fetchHomeData()
+    },[cityGeoLocation])
+
+
+    const fetchHomeData = async () => {
+        try {
+            setLoading(true)
+            const response = await fetch(url)
+            const data = await response.json()
+            // console.log(data)
+            setwhatsInYourMind(data.data.cards[0].card.card.imageGridCards.info)
+            setResturnatsChains(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+            setFoodDelivery(data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
+            
+        
+
+            setLoading(false)
+        }
+        catch (err) {
+            console.log(err)
+            setFail(true)
+        }
+
+    }
+
+
     return (
         <div>
             {
