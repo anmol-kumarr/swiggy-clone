778bd236918b045bbd8702427433c92e20302485
Anmol kumar <anmolkumar31884@gmail.com>
2024-07-31 12:32:34 +0530

    location slice DS changed

--- src/redux/locationSlice.js
+++ src/redux/locationSlice.js
@@ -8,6 +8,15 @@ const LocationSlice = createSlice({
             long: '77.594566',
             lat: '12.971599'
         },
+        {
+            city: [
+                {
+                    name: "Bengaluru",
+                    country: "IN",
+                    state: "Karnatka"
+                }
+            ]
+        }
     ]
     ,
     reducers: {
