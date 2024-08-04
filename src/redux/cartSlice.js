25771bcc99ba8c0161047c27dba20846888ad2bd
Anmol kumar <anmolkumar31884@gmail.com>
2024-08-04 21:43:01 +0530

    cart slice updated

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,18 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { SlActionRedo } from "react-icons/sl";
 
 const CartSlice = createSlice({
-    name: 'Cart',
+    name: 'cart',
     initialState: [],
     reducers: {
         addToCart: (state, action) => {
+
             state.push(action.payload)
         },
-        updateCart: (state, action) => {
-            return state.map((item) => (
-                item.id === action.payload.id ? { ...item, quantity: action.payload.quantity } :item 
-            ))
-        },
 
+        updateCart: (state, action) => {
+            console.log("id=", action.payload.itemId, "newQuantity=", action.payload.quantity);
+
+            const findIndex = state.findIndex(item => item.id === action.payload.itemId);
+            state[findIndex].quantity = action.payload.quantity;
+        
+        },
 
         removeFromCart: (state, action) => {
 
